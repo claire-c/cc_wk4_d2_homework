9c37fe9dcc098adadb22734121af92b5cc0a5b64
Claire <cconnachan@gmail.com>
2018-03-27 15:30:30 +0100

    added welcome erb file

--- controller.rb
+++ controller.rb
@@ -5,8 +5,12 @@ require_relative('./models/game.rb')
 
 get "/:player1/:player2" do
   game = Game.new(params[:player1], params[:player2])
-  winner = game.logic()
-  return "And the winner is... #{winner}"
-  # @winner = game.logic
-  # erb(:layout)
+  @winner = game.logic()
+  #return "And the winner is... #{winner}"
+  erb(:result)
+end
+
+get "/welcome" do
+erb(:welcome)
+
 end
